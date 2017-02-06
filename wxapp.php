881442a9feb25b026a8357a43906f15543516a24
chennl <chennlhz@126.com>
2017-02-06 22:34:59 +0800

    again token

--- wxapp.php
+++ wxapp.php
@@ -1,13 +1,12 @@
 <?php
-
 define("TOKEN", "7242609C8AEF41F88622A80AFCBE4E83");
-
 $wechatObj = new wechatCallbackapiHandler();
 if (!isset($_GET['echostr'])) {
     $wechatObj->responseMsg();
 }else{
     $wechatObj->valid();
 }
+
 
 class wechatCallbackapiHandler
 {
